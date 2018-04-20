2ec6ab428a4cded3923f0b3640d5553d5545ad00
akshat0123 <akshat0123@gmail.com>
2018-04-20 19:06:01 -0400

    Added front page to api

--- VendAPI/app.js
+++ VendAPI/app.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var index = require('./routes/index');
 var api = require('./routes/api');
 
 var app = express();
 
 // view engine setup
-app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,20 +22,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
 app.use('/api', api);
 
 // Postgres setup
 const pgp = require('pg-promise')();
-const cstr = { 
-	host: 'localhost', 
-	port: 5432, 
-	database: 'smart_vend', 
-	user: 'vend_admin', 
-	password: 'vend_admin' 
+const cstr = {
+	host: 'localhost',
+	port: 5432,
+	database: 'smart_vend',
+	user: 'vend_admin',
+	password: 'vend_admin'
 };
 const db = pgp(cstr);
 app.set('db', db);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- VendAPI/routes/api.js
+++ VendAPI/routes/api.js
@@ -6,7 +6,7 @@ router.post('/load', function(req, res, next) {
 	var db = req.app.get('db');
 	var data = req.body.data;
 	var machine_id = data.machine_id;
-	var item_id = item.item_id;
+	var item_id = data.item_id;
 	db.none('INSERT INTO machine_stock (machine_id, item_id) VALUES ($1, $2)', [machine_id, item_id]).then(() => {
 		console.log('Inserted item ' + item_id + ' into machine ' + machine_id);
 	});
