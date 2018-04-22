d02ad409bf2c0ecbc88b039fb475f771654b1603
akshat0123 <akshat0123@gmail.com>
2018-04-22 15:44:38 -0400

    Updated stock logs view | Added table to UI

--- VendAPI/routes/analytics.js
+++ VendAPI/routes/analytics.js
@@ -16,6 +16,21 @@ router.get('/revenue_pie', function(req, res, next) {
 
 });
 
+
+router.get('/stock_table', function(req, res, next) {
+	var db = req.app.get('db');
+
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+	var q = "SELECT month, location, item_name, count(item_name) FROM stock_logs GROUP BY month, location, item_name;";
+	db.any(q).then(data => {
+		res.json(data);
+	}).catch(err => {
+		console.log(err);
+	});
+
+});
   
 
 module.exports = router;
--- VendAnalytics/src/app/app.component.ts
+++ VendAnalytics/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
 
 	revenuePie() {
 
-		var url = 'http://localhost:3000/analytics/revenue_pie'
+		var url = 'http://localhost:3000/analytics/revenue_pie';
 		this.http.get(url).subscribe(rows => {
 			console.log(rows);
 
@@ -33,6 +33,47 @@ export class AppComponent implements OnInit {
 			Plotly.newPlot('chart', data, layout);
 
 		});
+	}
+
+	stockTable() {
+
+		var url = 'http://localhost:3000/analytics/stock_table';
+		this.http.get(url).subscribe(rows => {
+			
+			var months = [];
+			var locations = [];
+			var item_names = [];
+			var counts = [];
+			for (var row in rows) {
+				months.push(rows[row]['month']);
+				locations.push(rows[row]['location']);
+				item_names.push(rows[row]['item_name']);
+				counts.push(rows[row]['count']);
+			};
+
+			var rows_data = [ months, locations, item_names, counts ];
+
+			var data = [{
+				type: 'table',
+				header: {
+					values: [["Month"], ["Location"], ["Item Name"], ["Count"]],
+					align: "center",
+					line: {width: 1, color: 'black'},
+					fill: {color: "grey"},
+					font: {family: "Arial", size: 12, color: "white"}
+				},
+				cells: {
+					values: rows_data,
+					align: "center",
+					line: {color: "black", width: 1},
+					font: {family: "Arial", size: 11, color: ["black"]}
+				}
+			}]
+
+			Plotly.newPlot('chart', data);
+
+		});
+
 	}
 
 	ngOnInit() {
