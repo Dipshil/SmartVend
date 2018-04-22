86e9c402d76de6918b7d5224415cb376f94929f6
akshat0123 <akshat0123@gmail.com>
2018-04-22 00:20:41 -0400

    Updated UI | API now accepts calls from RPI | Charts generated from simulated data

--- VendAnalytics/src/app/app.component.ts
+++ VendAnalytics/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
 	selector: 'app-root',
@@ -11,9 +12,30 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class AppComponent implements OnInit {
 	title = 'app';
 
-	ngOnInit() {
+	constructor(private http: HttpClient) {} 
 
-		Plotly.newPlot('chart', [{ 'x': [1, 2, 3, 4, 5], 'y': [1, 2, 3, 4, 5] }]);
+	revenuePie() {
+
+		var url = 'http://172.26.135.37:3000/analytics/revenue_pie'
+		this.http.get(url).subscribe(rows => {
+			console.log(rows);
+
+			var values = []
+			var labels = []
+			for (var row in rows) {
+				values.push(rows[row]['sum']);
+				labels.push(rows[row]['item_name']);
+			}
+
+			var data = [{ values: values, labels: labels, type: 'pie' }];
+			var layout = { height: 800, width: 1000 };
+
+			Plotly.newPlot('chart', data, layout);
+
+		});
+	}
+
+	ngOnInit() {
 
 	}
 }
