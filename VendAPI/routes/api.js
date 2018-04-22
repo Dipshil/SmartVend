866aa095d4b56e44861726cb2e28fa34eb5e221d
akshat0123 <akshat0123@gmail.com>
2018-04-22 00:20:41 -0400

    Updated UI | API now accepts calls from RPI | Charts generated from simulated data

--- VendAPI/routes/api.js
+++ VendAPI/routes/api.js
@@ -4,9 +4,9 @@ var router = express.Router();
 // Load item into a machine's stock
 router.post('/load', function(req, res, next) {
 	var db = req.app.get('db');
-	var data = req.body.data;
-	var machine_id = data.machine_id;
-	var item_id = data.item_id;
+	console.log(req.body);
+	var machine_id = req.body.machine_id;
+	var item_id = req.body.item_id;
 	db.none('INSERT INTO machine_stock (machine_id, item_id) VALUES ($1, $2)', [machine_id, item_id]).then(() => {
 		console.log('Inserted item ' + item_id + ' into machine ' + machine_id);
 	}).catch(err => {
