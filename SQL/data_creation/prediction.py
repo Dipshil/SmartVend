14734940847613d8522655c6b9ed1cd14c5ebfc1
Shrirag <shriragk@gmail.com>
2018-04-24 23:00:59 -0400

    added normalization before MSE

--- SQL/data_creation/prediction.py
+++ SQL/data_creation/prediction.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 from filepaths import *
-from sklearn.metrics import mean_squared_error, explained_variance_score
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import normalize
 
 
 class PurchasePrediction:
@@ -40,10 +41,12 @@ class PurchasePrediction:
 
     def evaluate_model(self, start_month, num_test_elements):
         MSE = dict()
-        EVar = dict()
         for item in range(1, 15):
-            train_purchase_data = self.item_purchases_month[item][start_month:-num_test_elements]
-            test_purchase_ground_truth = self.item_purchases_month[item][-num_test_elements:]
+
+            norm_item_purchases_month = normalize(np.asarray(self.item_purchases_month[item]).reshape(1, -1)).ravel()
+            train_purchase_data = norm_item_purchases_month[start_month:-num_test_elements]
+
+            test_purchase_ground_truth = norm_item_purchases_month[-num_test_elements:].ravel()
 
             train_months = self.months[start_month:-num_test_elements] 
             test_months = self.months[-num_test_elements:]
@@ -54,16 +57,9 @@ class PurchasePrediction:
             predictions = [poly(month) for month in test_months]
 
             MSE[item] = mean_squared_error(test_purchase_ground_truth, predictions)
-            EVar[item] = explained_variance_score(test_purchase_ground_truth, predictions)
 
         with open(MSE_SCORES, 'w') as handle:
             for item in range(1,15):
                 handle.write("Item ID: %s, MSE: %s \n" %(item, str(MSE[item])))
 
-        with open(EVAR_SCORES, 'w') as handle:
-            for item in range(1,15):
-                handle.write("Item ID: %s, Explained Variation: %s \n" %(item, str(EVar[item])))
 
-
-
-            
